1c90a9ff3d35e89e189bdff905dd09169f97d92b
elbobo-binsh <151009490+elbobo-binsh@users.noreply.github.com>
2024-04-17 13:48:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hafta 6
+Görseldeki mobil uygulamanın kodlarını yazınız.
+![alt text](https://github.com/ezfesoft/mobil_w6/blob/main/flutter_w6_odev.png)
+
+
+## Mobil Programlama
+
+- Container
+- Text
+- Column
+- Icon
+- Row
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Uygulamam());
+}
+
+class Uygulamam extends StatelessWidget {
+  const Uygulamam({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mobil Hafta 6',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade300),
+        useMaterial3: true,
+      ),
+      home: const AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({super.key});
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Mobil Hafta 6"),
+      ),
+      body: Center(
+          child:
+              //--------------
+              //--------------
+              //--------------Kodları aşağıya yazınız.
+              //--------------
+              Column()
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------Kodları yukarıya yazınız.
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          ),
+    );
+  }
+}
